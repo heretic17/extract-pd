9522c77a27d762336026b134f7d6c2a874dfb2f1
Kaan <137988484+heresyglitched@users.noreply.github.com>
2024-05-09 14:45:14 +0300

    ss

--- extract.py
+++ extract.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import re
 
 # Read the Excel file, specifying the column range
-df = pd.read_excel('Active+Listings+Report+04-10-2024.xlsx', usecols="D:D", skiprows=0)
+df = pd.read_excel('Active+Listings+Report+04-12-2024.xlsx', usecols="D:D", skiprows=0)
 print(df)
 
 # Define a regular expression pattern to match the desired strings
@@ -22,6 +23,50 @@ filtered_df.to_csv('output/filtered_data.txt', index=False, header=False, sep='\
 # Write the filtered DataFrame to an Excel file
 filtered_df.to_excel('output/filtered_data.xlsx', index=False)
 
+# Convert the column values into a list if the DataFrame is not empty
+if not filtered_df.empty:
+    input_list = filtered_df.iloc[:, 0].tolist()  # Use tolist() to convert to list
+    print(input_list)
+else:
+    print("DataFrame is empty. No data read.")
+
+# Use regular expression to find the desired part from each string in the list
+pattern = r'\b\d+\b'  # Match one or more digits surrounded by word boundaries
+result_parts = [re.search(pattern, item).group() for item in input_list if re.search(pattern, item)]
+
+# print(result_parts)
+result_df = pd.DataFrame(result_parts, columns=['Result Parts'])
+result_df.to_excel('output/result_parts.xlsx', index=False)
+
+
+# Read the Excel file into a DataFrame
+
+# Extract column values as a list
+column_values = result_df['Result Parts'].tolist()
+
+def remove_duplicates(arr):
+    unique_nums = set()
+    result = []
+
+    for num in arr:
+        if num not in unique_nums:
+            result.append(num)
+            unique_nums.add(num)
+
+    return result
+
+# Remove duplicates from the column values
+column_values_without_duplicates = remove_duplicates(column_values)
+
+# Print array without duplicates
+print("Array without duplicates:", column_values_without_duplicates)
+
+# Create a DataFrame from the unique values
+removed_dupes_df = pd.DataFrame(column_values_without_duplicates, columns=['Removed_Parts'])
+removed_dupes_df = removed_dupes_df.astype(str)
+
+# Write the DataFrame to an Excel file
+removed_dupes_df.to_excel('output/removed_dupes.xlsx', index=False)
 
 # Old pattern 1: r'^(AMZ,|\b(?:VITA-|vita-|amz,))'
 # Old pattern 2: r'\b(?i:AMZ,|\s*VITA\s*-|vita\s*-|amz,)\s*'
--- remove_dupes.py
+++ remove_dupes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Excel file into a DataFrame
-df = pd.read_excel('output/result_parts.xlsx', usecols=["Result Parts"])
+df = pd.read_excel('output/result_parts.xlsx', usecols=["Result Parts"], dtype=str)
 
 # Extract column values as a list
 column_values = df['Result Parts'].tolist()
@@ -25,6 +25,7 @@ print("Array without duplicates:", column_values_without_duplicates)
 
 # Create a DataFrame from the unique values
 result_df = pd.DataFrame(column_values_without_duplicates, columns=['Removed_Parts'])
+result_df = result_df.astype(str)
 
 # Write the DataFrame to an Excel file
 result_df.to_excel('output/removed_dupes.xlsx', index=False)
